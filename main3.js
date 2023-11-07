43bc4bf2dd689f88c8acf884898fe2cf9310e6e3
hashimoto <eqs.hashimoto@gmail.com>
2023-11-07 15:04:02 +0900

    不要なやつも追加

--- main3.js
+++ main3.js
@@ -1,45 +1,14 @@
 function postEl(text) {
   return `<div>${text}</div>`;
 }
-// console.log(11);
 
-// function btnClick() {
-//   let postsElm = document.getElementsByClassName('posts');
-//   // let postsElm = document.getElementById('posts');
-//   //indexをつけたらいけた
-//   postsElm[0].textContent = "こんにちは";
-//   console.log(postsElm);
-//   // 指定した要素を取得
-// let obj = document.getElementById("sample");
-
-// // console.log(obj.textContent) // 要素
-
-// // 変更
-// obj.textContent = "変更";
-// }
-
-//初期投稿の設定
+// 最初の投稿の設定
 function setupInitialPosts() {  
-  // console.log(Array(100));  //[empty × 100]
-  // console.log(...Array(100)); //[undefined, * 100]
-  // console.log(...Array(100).keys()); // 0から99
-  // console.log(Array(100).keys()); // Array Iterator {}が出てきた
-  // console.log(document.getElementsByClassName('.posts').append(postEl("aa")));//エラー
-  console.log();
-
   [...Array(100).keys()].forEach(i => {
-    // post0から99までtextに入れる
     const text = `post${i}`
-    //改良：おそらく<div>にpost0~99を入れる
-    // $('.posts').append(postEl(text));//編集前
-    // console.log(document.getElementsByClassName('.posts')); //HTMLCollection []がズラーっと出てくる
-    document.getElementsByClassName('posts')[i].append(postEl(text));
-    // querySelector('.foo')
-    // console.log(document.getElementsByClassName('.posts').append(postEl(text)));  //Uncaught TypeError: document.getElementsByClassName(...).append is not a function　→エラーになってしまう
+    $('.posts').append(postEl(text));
   });
-  //scrollIntoViewは要素の下端が下に来るように表示する。
-  // $('.posts')[0].scrollIntoView(false);//編集前
-  document.getElementsByClassName('posts')[0].scrollIntoView(false);
+  $('.posts')[0].scrollIntoView(false);
 }
 
 function loadPrepend() {
@@ -66,12 +35,7 @@ function onScroll() {
   }, 200)
 }
 
-//編集後 ロード時に実行されるように
-window.onload = onLoad;
-function onLoad() {
-// $(() => {
+$(() => {
   setupInitialPosts();
   $(document).scroll(onScroll);
-};
-// });
-
+});
