3d1db025a03c1d3800af3fd9a5ca0e2c6053de66
hashimoto <eqs.hashimoto@gmail.com>
2023-11-10 10:25:13 +0900

    しゅうせい

--- main.js
+++ main.js
@@ -1,39 +1,41 @@
 // 以下JSONからデータを取得する。
 var sentMessageJSON;
 $.ajax({
-  url: 'sentMessageHistory.json',
-  type: 'GET',
+  url: "sentMessageHistory.json",
+  type: "GET",
   dataType: "json",
   //リクエストが完了するまで実行。
   beforeSend: function () {
     // ローディング画像を表示
-    $('.loading').removeClass('hide');
-  }
+    $(".loading").removeClass("hide");
+  },
   //成功。
-}).done(function (data) {
-  setTimeout(() => {
-    sentMessageJSON = data;
-    scrollFirstDisplay();
-    infiniteScroll();
-  }, "1999");
-  setTimeout(() => {
-    // ローディング画像を非表示
-    $('.loading').addClass('hide');
-    canPressButton();
-  }, "2000");
-//失敗。
-}).fail(function(){
-  console.log('error');
-});
+})
+  .done(function (data) {
+    setTimeout(() => {
+      sentMessageJSON = data;
+      scrollFirstDisplay();
+      infiniteScroll();
+    }, "1999");
+    setTimeout(() => {
+      // ローディング画像を非表示
+      $(".loading").addClass("hide");
+      canPressButton();
+    }, "2000");
+    //失敗。
+  })
+  .fail(function () {
+    console.log("error");
+  });
 // 以上JSONからデータを取得する。
 
 // .scrollを取得
-var scroll = document.querySelectorAll('.scroll');
+var scroll = document.querySelectorAll(".scroll");
 
 // 以下スクロール画面（.scroll）内にコンテンツ（画像かテキスト）を表示させる
 function scrollFirstDisplay() {
   //scrollは配列になっているため、ループ処理で記載する（一回しか行わないが）。
-  scroll.forEach(elm => {
+  scroll.forEach((elm) => {
     //JSONに記載されているIdを定義
     var sentId;
     //.scrollの子要素の高さの合計を定義
@@ -43,26 +45,27 @@ function scrollFirstDisplay() {
       // .scrollのlastnumを+1する
       elm.dataset.lastnum = parseInt(elm.dataset.lastnum) + 1;
       // JSONのId（sentId）を+1する
-      sentId = ('0000' + elm.dataset.lastnum).slice(-4);
+      sentId = ("0000" + elm.dataset.lastnum).slice(-4);
       // 以下JSONにテキストが記載されていた場合テキストを入れる
       if (sentMessageJSON[sentId].text != "") {
-        let div = document.createElement('div');
+        let div = document.createElement("div");
         //作成したdiv要素にテキストを入れる
         div.textContent = sentMessageJSON[sentId].text;
-        if (sentMessageJSON[sentId].whoSend == "receiver") {
-          div.classList.add("scroll--output_receiver__text");
-        } else if (sentMessageJSON[sentId].whoSend == "sender") {
-          div.classList.add("scroll--output_sender__text");
+        // メッセージの送り手がが
+        if (sentMessageJSON[sentId].whoSend == "myself") {
+          div.classList.add("scroll--output_myself__text");
+        } else if (sentMessageJSON[sentId].whoSend == "partner") {
+          div.classList.add("scroll--output_partner__text");
         }
         elm.prepend(div);
       }
       // 以下JSONに画像が記載されていた場合画像を入れる
       if (sentMessageJSON[sentId].img != "") {
-        let img = document.createElement('img');
-        if (sentMessageJSON[sentId].whoSend == "receiver") {
-          img.classList.add("scroll--output_receiver__img");
-        } else if (sentMessageJSON[sentId].whoSend == "sender") {
-          img.classList.add("scroll--output_sender__img");
+        let img = document.createElement("img");
+        if (sentMessageJSON[sentId].whoSend == "myself") {
+          img.classList.add("scroll--output_myself__img");
+        } else if (sentMessageJSON[sentId].whoSend == "partner") {
+          img.classList.add("scroll--output_partner__img");
         }
         img.src = sentMessageJSON[sentId].img;
         elm.prepend(img);
@@ -71,7 +74,7 @@ function scrollFirstDisplay() {
       childTotalHeight += elm.children[0].scrollHeight;
     }
     //.scrollの先頭（.scrollの一番下）に自動でスクロールする
-    $('.scroll')[0].lastElementChild.scrollIntoView(false);
+    $(".scroll")[0].lastElementChild.scrollIntoView(false);
   });
 }
 // 以下スクロール画面（.scroll）内にコンテンツ（画像かテキスト）を表示させる
@@ -79,7 +82,7 @@ function scrollFirstDisplay() {
 // 以下無限スクロール（上スクロール）
 function infiniteScroll() {
   //scrollは配列になっているため、ループ処理で記載する（一回しか行わないが）。
-  scroll.forEach(elm => {
+  scroll.forEach((elm) => {
     // スクロールされた際に以下の関数を実行
     elm.onscroll = function () {
       // .scrollIntoView(false)により、最初に元の位置（scrollTopが0）から下にスクロールされているので、上部までスクロールされたことを検知するために200より小さくなったときに処理を行う。
@@ -91,35 +94,35 @@ function infiniteScroll() {
       //以下上部までスクロールした際の処理
       this.dataset.lastnum = parseInt(this.dataset.lastnum) + 1;
       // JSONのId（sentId）を+1する
-      var sentId = ('0000' + this.dataset.lastnum).slice(-4);
+      var sentId = ("0000" + this.dataset.lastnum).slice(-4);
 
       // 以下テキストを入れる
       if (sentMessageJSON[sentId].text != "") {
-        let div = document.createElement('div');
+        let div = document.createElement("div");
         //作成したdiv要素にテキストを入れる
         div.textContent = sentMessageJSON[sentId].text;
-        if (sentMessageJSON[sentId].whoSend == "receiver") {
-          div.classList.add("scroll--output_receiver__text");
-        } else if (sentMessageJSON[sentId].whoSend == "sender") {
-          div.classList.add("scroll--output_sender__text");
+        if (sentMessageJSON[sentId].whoSend == "myself") {
+          div.classList.add("scroll--output_myself__text");
+        } else if (sentMessageJSON[sentId].whoSend == "partner") {
+          div.classList.add("scroll--output_partner__text");
         }
         this.prepend(div);
       }
 
       // 以下画像を入れる
       if (sentMessageJSON[sentId].img != "") {
-        let img = document.createElement('img');
-        if (sentMessageJSON[sentId].whoSend == "receiver") {
-          img.classList.add("scroll--output_receiver__img");
-        } else if (sentMessageJSON[sentId].whoSend == "sender") {
-          img.classList.add("scroll--output_sender__img");
+        let img = document.createElement("img");
+        if (sentMessageJSON[sentId].whoSend == "myself") {
+          img.classList.add("scroll--output_myself__img");
+        } else if (sentMessageJSON[sentId].whoSend == "partner") {
+          img.classList.add("scroll--output_partner__img");
         }
         img.src = sentMessageJSON[sentId].img;
         this.prepend(img);
       }
     };
   });
-};
+}
 // 以上無限スクロール（上スクロール）
 
 const sendbutton = document.getElementById("sendbutton");
@@ -130,45 +133,51 @@ function canPressButton() {
 // 以上ボタンを押せるようにする
 
 // 以下入力してボタンを押すとテキストが表示される
-sendbutton.addEventListener('click', function () {
+sendbutton.addEventListener("click", function () {
+  //以下送信ボタンを押した際に日時を表示
+  // span要素を格納
+  let timeElm = document.createElement("span");
+  // 現在時刻を取得
+  let posttime = new Date();
+  console.log(posttime);
+  let postHours = posttime.getHours() + 1;
+  let postMinutes = posttime.getMinutes();
+  let postSeconds = posttime.getSeconds();
+  // 現在時刻をHTMLに挿入(入力値と一緒にまとめて.scrollの子要素に追加)
+  timeElm.innerHTML = postHours + ":" + postMinutes + ":" + postSeconds;
+  //以上送信ボタンを押した際に日時を表示
+
   // 入力値を取得する
   var inputText = document.getElementById("inputText").value;
   // 改行を<br>に変換するindention()を呼ぶ
-	inputText = indention(inputText);
-	// inputタグに入力したテキストを削除する（送信ボタンを押すと消えるようにする）
-	document.getElementById("inputText").value = "";
+  inputText = indention(inputText);
+  // inputタグに入力したテキストを削除する（送信ボタンを押すと消えるようにする）
+  document.getElementById("inputText").value = "";
 
-	// 取得した入力値を表示させるdiv要素を作成
-	var outputText = document.createElement('div');
-	// 作成したdiv要素にclassをつける
-	outputText.classList.add("scroll--output_receiver__text");
-	//.scrollが付いている要素のノードリストを取得する
-	var scroll = document.querySelectorAll('.scroll');
-	scroll.forEach(elm => {
-		// .scrollが付いている要素の子要素の先頭に作成したdivタグを入れる
-		elm.append(outputText);
+  // 取得した入力値を表示させるdiv要素を作成
+  var outputText = document.createElement("div");
+  // 作成したdiv要素にclassをつける
+  outputText.classList.add("scroll--output_myself__text");
+  //.scrollが付いている要素のノードリストを取得する
+  var scroll = document.querySelectorAll(".scroll");
+  scroll.forEach((elm) => {
     // 入力したテキストを作成したoutputText(divタグ)に入れる
-		outputText.innerHTML = inputText;
+    outputText.innerHTML = timeElm.innerHTML + "<span>" + inputText + "</span>";
+    // .scrollが付いている要素の子要素の先頭に作成したdivタグを入れる
+    elm.append(outputText);
   });
   // テキストを送信した際に一番下（テキストを送信したかわかるように）に移動するようにする
-  $('.scroll')[0].lastElementChild.scrollIntoView(false);
+  $(".scroll")[0].lastElementChild.scrollIntoView(false);
 });
 // 以上入力してボタンを押すとテキストが表示される
 
 // 以下改行\nを<br>に置換、エスケープ処理
 function indention(a) {
-    // 入力された文字
-    a = a.replace(/&/g, "&amp;");
-    a = a.replace(/</g, "&lt;");
-    a = a.replace(/>/g, "&gt;");
-    // 改行を置換する
-  var b = a.replace(/\n/g, '<br>');
-	return b;
-};
-
-//以下テキストエリアの高さを調整する
-var textarea = document.querySelector("textarea");
-textarea.addEventListener("input", function () {
-  this.style.height = "1em"; // 初期高さに戻す
-  this.style.height = this.scrollHeight + "px"; // スクロール領域の高さに合わせる
-});
+  // 入力された文字
+  a = a.replace(/&/g, "&amp;");
+  a = a.replace(/</g, "&lt;");
+  a = a.replace(/>/g, "&gt;");
+  // 改行を置換する
+  var b = a.replace(/\n/g, "<br>");
+  return b;
+}
